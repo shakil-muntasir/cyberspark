ee48cb9e7cb0815702ecf70af897c0bb99851dc8
Muntasir Rahman <shakil_muntasir@hotmail.com>
2024-08-15 09:02:19 +0600

    Server side image resize

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,9 +7,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ProfileController extends Controller
 {
@@ -33,9 +37,34 @@ class ProfileController extends Controller
 
         $image = $request->file('image');
         if ($image) {
-            $imageName = "user-{$request->user()->id}.{$image->getClientOriginalExtension()}";
+            // Define user-specific directory and random filename
+            $userId = $request->user()->id;
+            $directory = base_path("public/storage/users/{$userId}");
 
-            $data['image'] = $image->storeAs('users', $imageName, 'public');
+            // Empty the directory before saving the new image
+            if (File::exists($directory)) {
+                $files = File::files($directory);
+                foreach ($files as $file) {
+                    File::delete($file);
+                }
+            } else {
+                // If directory does not exist, create it
+                File::makeDirectory($directory, 0755, true); // Creates the directory with the necessary permissions
+            }
+
+            // Read image from file system and resize it
+            $manager = new ImageManager(new Driver());
+            $resizedImage = $manager->read($image)->coverDown(250, 250, 'center')->toJpeg(80);
+
+            // Generate a random filename
+            $randomFileName = Str::random(24) . '.jpg'; // or any other extension depending on image format
+            $path = $directory . '/' . $randomFileName;
+
+            // Save the resized image
+            $resizedImage->save($path);
+
+            // Save the image path to the data array
+            $data['image'] = "users/{$userId}/{$randomFileName}";
         }
 
         $request->user()->fill($data);
@@ -46,7 +75,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit');
+        return redirect()->back();
     }
 
     /**
