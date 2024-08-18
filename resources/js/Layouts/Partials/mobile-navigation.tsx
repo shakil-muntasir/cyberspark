1454f7e7286da9fae6f3274d499cc8e4d754a7cd
Sayok Zaman <sayok.zaman.sz@gmail.com>
2024-08-19 02:22:48 +0600

    Mobile nav active state

--- resources/js/Layouts/Partials/mobile-navigation.tsx
+++ resources/js/Layouts/Partials/mobile-navigation.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@inertiajs/react'
+import { Link, usePage } from '@inertiajs/react'
 import { PanelLeftIcon } from 'lucide-react'
 
 import { routes } from '@/Lib/routes'
@@ -9,35 +9,35 @@ import { SheetTrigger, SheetContent, Sheet, SheetClose } from '@/Components/ui/s
 import SprintDevsLogo from '@/public/assets/sprint_devs.png'
 
 export default function MobileNavigation() {
-    const pathname = route().current()
+  const pathname = usePage().url.split('?')[0]
 
-    return (
-        <Sheet>
-            <SheetTrigger asChild>
-                <Button className='sm:hidden' size='icon' variant='outline'>
-                    <PanelLeftIcon className='h-5 w-5' />
-                    <span className='sr-only'>Toggle Menu</span>
-                </Button>
-            </SheetTrigger>
-            <SheetContent className='sm:max-w-xs' side='left'>
-                <nav className='grid gap-6 text-lg font-medium'>
-                    <SheetClose asChild>
-                        <Link className='group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-gray-100 text-lg font-semibold text-primary-foreground md:text-base' href='/'>
-                            <img src={SprintDevsLogo} alt='Sprint Devs' className='rounded-full transition-all group-hover:scale-110' />
-                            <span className='sr-only'>Sprint Devs</span>
-                        </Link>
-                    </SheetClose>
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button className='sm:hidden' size='icon' variant='outline'>
+          <PanelLeftIcon className='h-5 w-5' />
+          <span className='sr-only'>Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent className='sm:max-w-xs' side='left'>
+        <nav className='grid gap-2 text-lg font-medium'>
+          <SheetClose asChild>
+            <Link className='group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-gray-100 text-lg font-semibold text-primary-foreground md:text-base mb-2' href='/'>
+              <img src={SprintDevsLogo} alt='Sprint Devs' className='rounded-full transition-all group-hover:scale-110' />
+              <span className='sr-only'>Sprint Devs</span>
+            </Link>
+          </SheetClose>
 
-                    {routes.map(route => (
-                        <SheetClose key={route.href} asChild>
-                            <Link key={route.href} className={`flex items-center gap-4 px-2.5 transition-colors ${pathname === route.href ? 'text-foreground' : 'text-muted-foreground hover:text-foreground'}`} href={route.href}>
-                                <route.icon className='h-5 w-5' />
-                                {route.tooltip}
-                            </Link>
-                        </SheetClose>
-                    ))}
-                </nav>
-            </SheetContent>
-        </Sheet>
-    )
+          {routes.map(route => (
+            <SheetClose key={route.href} asChild>
+              <Link key={route.href} className={`flex items-center gap-4 px-2.5 py-2 rounded-md transition-colors ${pathname === route.href || pathname.startsWith(`${route.href}/`) ? 'bg-accent text-foreground' : 'text-muted-foreground hover:text-foreground'}`} href={route.href}>
+                <route.icon className='h-5 w-5' />
+                {route.tooltip}
+              </Link>
+            </SheetClose>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  )
 }
