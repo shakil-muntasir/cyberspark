6610ae2d45a2aa40f3535dd44f816a48160d9023
Sayok Zaman <sayok.zaman.sz@gmail.com>
2024-08-19 11:29:49 +0600

    Laravel Clockwork

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,6 @@
 import '../css/app.css'
 import './bootstrap'
+import 'clockwork-browser/toolbar'
 
 import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
