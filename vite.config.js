391bee36bd1fa43c70134cb88633099c24007b4f
Muntasir Rahman <shakil_muntasir@hotmail.com>
2024-09-24 02:04:06 +0600

    Suppress vite build file outputs

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
@@ -8,12 +9,36 @@ export default defineConfig({
       input: 'resources/js/app.tsx',
       refresh: true
     }),
-    react()
+    react(),
+    {
+      name: 'suppress-file-output',
+      apply: 'build',
+      buildEnd() {
+        const log = console.log
+        console.log = (...args) => {
+          const msg = args[0]
+          if (typeof msg === 'string' && (msg.includes('assets/') || msg.includes('manifest.json'))) return
+          log(...args)
+        }
+      }
+    }
   ],
   resolve: {
     alias: {
       '@/': '/resources/js',
       '@/public': '/public'
+    }
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      onwarn(warning, defaultHandler) {
+        if (warning.code === 'SOURCEMAP_ERROR') {
+          return
+        }
+
+        defaultHandler(warning)
+      }
     }
   }
 })
