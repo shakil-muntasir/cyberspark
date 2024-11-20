18919263920c628f55cf7ab3c11fcc4885e7a533
Muntasir Rahman <shakil_muntasir@hotmail.com>
2024-11-20 06:16:46 +0600

    Login page reworked

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -7,6 +7,8 @@ import InputError from '@/Components/InputError'
 import { Button } from '@/Components/ui/button'
 import { Input } from '@/Components/ui/input'
 import { Label } from '@/Components/ui/label'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/Components/ui/card'
+import NGOFLogo from '@/public/assets/NGOF_Logo_white.png'
 
 interface LoginForm {
   email: string
@@ -36,50 +38,60 @@ export default function Login() {
   return (
     <GuestLayout title='Login'>
       <div className='flex h-screen items-center justify-center py-12'>
-        <div className='mx-auto grid w-[350px] gap-6'>
-          <div className='grid gap-2 text-center'>
-            <h1 className='text-3xl font-bold'>Login</h1>
-            <p className='w-full text-balance text-muted-foreground lg:text-nowrap'>Enter your email below to login to your account</p>
-          </div>
-
-          <form onSubmit={handleLoginSubmit} className='grid gap-3'>
-            <div className='grid gap-2'>
-              <Label htmlFor='email' className={errors.email?.length ? 'text-destructive' : ''}>
-                Email
-              </Label>
-              <div className='space-y-px'>
-                <Input id='email' type='text' name='email' value={data.email} onChange={handleInputChange} tabIndex={1} placeholder='your@email.com' autoComplete='email' />
-                <InputError message={errors.email} />
+        <Card className='border-0 lg:border'>
+          <CardHeader className='flex items-center justify-center'>
+            <CardTitle>
+              <img src={NGOFLogo} className='-ml-1.5 h-16 object-contain' />
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className='mx-auto grid w-[350px] gap-6'>
+              <div className='grid gap-2 text-center'>
+                <h1 className='text-2xl font-semibold'>NGO Forum ERP</h1>
+                <p className='mt-2 w-full text-balance text-muted-foreground lg:text-nowrap'>Enter your email below to login to your account</p>
               </div>
+
+              <form onSubmit={handleLoginSubmit} className='grid gap-3'>
+                <div className='grid gap-2'>
+                  <Label htmlFor='email' className={errors.email?.length ? 'text-destructive' : ''}>
+                    Email
+                  </Label>
+                  <div className='space-y-px'>
+                    <Input id='email' type='text' name='email' value={data.email} onChange={handleInputChange} tabIndex={1} placeholder='your@email.com' autoComplete='email' />
+                    <InputError message={errors.email} />
+                  </div>
+                </div>
+
+                <div className='grid gap-2'>
+                  <div className='flex items-center'>
+                    <Label htmlFor='password' className={errors.password?.length ? 'text-destructive' : ''}>
+                      Password
+                    </Label>
+                    <Link href='#' className='ml-auto inline-block text-sm underline'>
+                      Forgot your password?
+                    </Link>
+                  </div>
+                  <div className='space-y-px'>
+                    <Input id='password' type='password' name='password' value={data.password} onChange={handleInputChange} tabIndex={2} placeholder='••••••••' autoComplete='current-password' />
+                    <InputError message={errors.password} />
+                  </div>
+                </div>
+
+                <Button type='submit' className='w-full' tabIndex={3}>
+                  Login
+                </Button>
+              </form>
             </div>
-
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password' className={errors.password?.length ? 'text-destructive' : ''}>
-                  Password
-                </Label>
-                <Link href='#' className='ml-auto inline-block text-sm underline'>
-                  Forgot your password?
-                </Link>
-              </div>
-              <div className='space-y-px'>
-                <Input id='password' type='password' name='password' value={data.password} onChange={handleInputChange} tabIndex={2} placeholder='••••••••' autoComplete='current-password' />
-                <InputError message={errors.password} />
-              </div>
+          </CardContent>
+          <CardFooter>
+            <div className='text-center text-sm'>
+              Don&apos;t have an account?{' '}
+              <Link href='/register' className='underline'>
+                Sign up
+              </Link>
             </div>
-
-            <Button type='submit' className='w-full' tabIndex={3}>
-              Login
-            </Button>
-          </form>
-
-          <div className='mt-4 text-center text-sm'>
-            Don&apos;t have an account?{' '}
-            <Link href='/register' className='underline'>
-              Sign up
-            </Link>
-          </div>
-        </div>
+          </CardFooter>
+        </Card>
       </div>
     </GuestLayout>
   )
